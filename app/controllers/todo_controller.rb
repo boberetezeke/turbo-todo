0d500ee0b0ea1d9baae9699e3927cb7073220070
Steve Tuckner <stevetuckner@gmail.com>
2026-04-29 20:12:19 -0500

    added ability to add todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,37 @@
 class TodoController < ApplicationController
   def index
-    @todos = Todo.all
+    @todos = ordered_todos
     @new_todo = Todo.new
+  end
+
+  def create
+    @new_todo = Todo.new(todo_params)
+
+    if @new_todo.save
+      @created_todo = @new_todo
+      @new_todo = Todo.new
+
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream { render :create, status: :created }
+      end
+    else
+      @todos = ordered_todos
+
+      respond_to do |format|
+        format.html { render :index, status: :unprocessable_entity }
+        format.turbo_stream { render :create, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def ordered_todos
+    Todo.order(:id)
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 end
